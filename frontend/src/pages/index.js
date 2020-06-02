5c5d5ff08dd0d5c2a03c4cfee2e32ad7b8f703ed
Clare <chblackburne@gmail.com>
2020-06-02 15:15:46 +0100

    show movies and posters on index

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import MovieList from "../components/movie-list"
 import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => {
@@ -10,14 +10,8 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
+      <MovieList data={data} />
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </Layout>
   )
 }
