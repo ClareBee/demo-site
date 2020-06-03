813598ad05fef9ec623420700c756fc550de20f3
Clare <chblackburne@gmail.com>
2020-06-03 16:01:49 +0100

    alphabetise list

--- frontend/src/components/movie-list.js
+++ frontend/src/components/movie-list.js
@@ -3,11 +3,18 @@ import styles from "./movie-list.module.css"
 import MovieItem from "./movie-item"
 
 const MovieList = ({ data }) => {
+  const sortByTitle = movies => {
+    console.log(movies)
+    const sorted = movies.sort((a, b) =>
+      a.node.title.localeCompare(b.node.title)
+    )
+    return sorted
+  }
   return (
     <article>
       <ul className={styles.list}>
-        {data.allSanityMovie.edges.map(({ node }) => (
-          <MovieItem node={node} />
+        {sortByTitle(data.allSanityMovie.edges).map(({ node }) => (
+          <MovieItem node={node} key={node.title} />
         ))}
       </ul>
     </article>
